6dee3440b5604c160167dbc6bac9229464c70323
ElCzar <cesarandresolarte@gmail.com>
2025-08-19 12:29:12 -0500

    feat: initial base commit

---
+++ src/main/java/co/edu/puj/secchub_backend/SecchubBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.puj.secchub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecchubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecchubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/puj/secchub_backend/SecchubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.puj.secchub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecchubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
