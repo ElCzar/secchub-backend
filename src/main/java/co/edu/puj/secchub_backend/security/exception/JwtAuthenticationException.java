5d45f29247108a406c35cf7a60b9adbb38562e3a
Cesar Olarte <104476524+ElCzar@users.noreply.github.com>
2025-09-01 23:51:58 -0500

    Revert "feat: add authentication and authorization to application"

--- src/main/java/co/edu/puj/secchub_backend/security/exception/JwtAuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package co.edu.puj.secchub_backend.security.exception;
-
-public class JwtAuthenticationException extends RuntimeException {
-    public JwtAuthenticationException(String message) {
-        super(message);
-    }
-}
