317f4bc3d816cd1b5721bc818c0d58a4c46106bd
Cesar Olarte <104476524+ElCzar@users.noreply.github.com>
2025-09-02 19:23:34 -0500

    fix: add SpringBootApplication annotation to main class

--- src/main/java/co/edu/puj/secchub_backend/SecchubBackendApplication.java
+++ src/main/java/co/edu/puj/secchub_backend/SecchubBackendApplication.java
@@ -3,6 +3,7 @@ package co.edu.puj.secchub_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SecchubBackendApplication {
 
     public static void main(String[] args) {
