b9d1b2a5027fa9240dbad5e220c29f1aa9b3f819
richie <richiegut30@gmail.com>
2025-10-08 20:25:12 -0500

    fix: make findAllCourses reactive

--- src/main/java/co/edu/puj/secchub_backend/admin/controller/CourseController.java
+++ src/main/java/co/edu/puj/secchub_backend/admin/controller/CourseController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 import java.util.Map;
@@ -42,8 +43,9 @@ public class CourseController {
      */
     @GetMapping
     @PreAuthorize("isAuthenticated()")
-    public List<CourseResponseDTO> findAllCourses() {
-        return courseService.findAllCourses();
+    public Flux<CourseResponseDTO> findAllCourses() {
+        return Flux.defer(() -> Flux.fromIterable(courseService.findAllCourses()))
+                .subscribeOn(reactor.core.scheduler.Schedulers.boundedElastic());
     }
 
     /**
