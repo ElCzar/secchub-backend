fb50da7a50db433a6e6eb33104e6d23bd3f5de21
Cesar Andres Olarte <cesarandresolarte@gmail.com>
2025-10-13 16:26:13 -0500

    feat: add user information endpoint with controller and service

---
+++ src/main/java/co/edu/puj/secchub_backend/security/controller/UserController.java
@@ -0,0 +1,62 @@
+package co.edu.puj.secchub_backend.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.puj.secchub_backend.security.dto.UserInformationResponseDTO;
+import co.edu.puj.secchub_backend.security.service.UserService;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/**
+ * REST controller for managing user-information related endpoints.
+ * Provides endpoints to retrieve user details.
+ */
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    /**
+     * Get logged-in user information.
+     * @return ResponseEntity<UserInformationResponseDTO> with user details and code 200
+     */
+    @GetMapping
+    @PreAuthorize("isAuthenticated()")
+    public Mono<ResponseEntity<UserInformationResponseDTO>> getUserInformation() {
+        return userService.getUserInformation()
+                .map(ResponseEntity::ok);
+    }
+    
+    /**
+     * Get user information by email.
+     * @param email User email
+     * @return ResponseEntity<UserInformationResponseDTO> with user details and code 200
+     */
+    @GetMapping("/email")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+    public Mono<ResponseEntity<UserInformationResponseDTO>> getUserInformationByEmail(@RequestParam String email) {
+        return userService.getUserInformationByEmail(email)
+                .map(ResponseEntity::ok);
+    }
+
+    /**
+     * Get user information by user ID.
+     * @param userId User ID
+     * @return ResponseEntity<UserInformationResponseDTO> with user details and code 200
+     */
+    @GetMapping("/id/{userId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+    public Mono<ResponseEntity<UserInformationResponseDTO>> getUserInformationById(@PathVariable Long userId) {
+        return userService.getUserInformationById(userId)
+                .map(ResponseEntity::ok);
+    }
+}
--- src/main/java/co/edu/puj/secchub_backend/security/service/UserService.java
+++ src/main/java/co/edu/puj/secchub_backend/security/service/UserService.java
@@ -2,15 +2,20 @@ package co.edu.puj.secchub_backend.security.service;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Service;
 
 import co.edu.puj.secchub_backend.security.contract.SecurityModuleUserContract;
 import co.edu.puj.secchub_backend.security.contract.UserCreationRequestDTO;
+import co.edu.puj.secchub_backend.security.dto.UserInformationResponseDTO;
 import co.edu.puj.secchub_backend.security.exception.UserNotFoundException;
 import co.edu.puj.secchub_backend.security.model.User;
 import co.edu.puj.secchub_backend.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Service
 @Slf4j
@@ -37,5 +42,41 @@ public class UserService implements SecurityModuleUserContract {
         user.setPassword(passwordEncoderService.encode(user.getPassword()));
         User savedUser = userRepository.save(user);
         return savedUser.getId();
+    }
+
+    /**
+     * Gets the userInformation by userId
+     * @param userId the user ID
+     * @return UserInformationResponseDTO with user details
+     */
+    public Mono<UserInformationResponseDTO> getUserInformationById(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found for id: " + userId));
+        return Mono.just(modelMapper.map(user, UserInformationResponseDTO.class));
+    }
+
+    /**
+     * Gets the userInformation by email
+     * @param email the user email
+     * @return UserInformationResponseDTO with user details
+     */
+    public Mono<UserInformationResponseDTO> getUserInformationByEmail(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("User not found for email: " + email));
+        return Mono.just(modelMapper.map(user, UserInformationResponseDTO.class));
+    }
+
+    /**
+     * Gets the logged-in user information
+     * @return UserInformationResponseDTO with user details
+     */
+    public Mono<UserInformationResponseDTO> getUserInformation() {
+        return ReactiveSecurityContextHolder.getContext()
+                .flatMap(securityContext -> Mono.fromCallable(() ->{
+                    String authenticatedUserId = securityContext.getAuthentication().getName();
+                    User user = userRepository.findByEmail(authenticatedUserId)
+                            .orElseThrow(() -> new UserNotFoundException("User not found for email: " + authenticatedUserId));
+                    return modelMapper.map(user, UserInformationResponseDTO.class);
+                }).subscribeOn(Schedulers.boundedElastic()));
     }
 }
